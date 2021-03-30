e4dc86cb6d94da9bf8dc0cf4dc7ddc5e7e243df8
Maxim Chervonny <fiskus@chervonny.ru>
2021-03-30 10:35:22 +0300

    use dict instead of array

--- catalog/app/utils/packageHandle.spec.ts
+++ catalog/app/utils/packageHandle.spec.ts
@@ -1,17 +1,6 @@
 import * as packageHandle from './packageHandle'
 
 describe('utils/packageHandle', () => {
-  describe('getValueName', () => {
-    it('should return value name', () => {
-      // eslint-disable-next-line no-template-curly-in-string
-      expect(packageHandle.getValueName('${{ username }}')).toBe('username')
-    })
-
-    it('should return null if no value found', () => {
-      expect(packageHandle.getValueName('${{ username')).toBe(null)
-    })
-  })
-
   describe('convertItem', () => {
     it('should return static string when no template', () => {
       expect(packageHandle.convertItem('fgsfds')).toBe('fgsfds')
@@ -19,80 +8,83 @@ describe('utils/packageHandle', () => {
     })
 
     it('should treat broken template as a string and return this string', () => {
-      expect(packageHandle.convertItem('start and ${{ no end')).toBe(
-        'start and ${{ no end',
+      expect(packageHandle.convertItem('start and <%= no end')).toBe(
+        'start and <%= no end',
       )
     })
 
     it('should return converted when template has values', () => {
       expect(
         packageHandle.convertItem(
-          // eslint-disable-next-line no-template-curly-in-string
-          'what-${{ username }}-do/make-${{ directory }}-update',
+          'what-<%= username %>-do/make-<%= directory %>-update',
           { directory: 'staging', username: 'fiskus' },
         ),
       ).toBe('what-fiskus-do/make-staging-update')
       expect(
-        packageHandle.convertItem(
-          // eslint-disable-next-line no-template-curly-in-string
-          '${{ username }}/${{ directory }}',
-          { directory: 'staging', username: 'fiskus' },
-        ),
+        packageHandle.convertItem('<%= username %>/<%= directory %>', {
+          directory: 'staging',
+          username: 'fiskus',
+        }),
       ).toBe('fiskus/staging')
     })
 
     it('should return null when no values ', () => {
       expect(
         packageHandle.convertItem(
-          // eslint-disable-next-line no-template-curly-in-string
-          'what-${{ username }}-do/make-${{ directory }}-update',
+          'what-<%= username %>-do/make-<%= directory %>-update',
           { username: 'fiskus' },
         ),
       ).toBe(null)
       expect(
-        packageHandle.convertItem(
-          // eslint-disable-next-line no-template-curly-in-string
-          '${{ username }}/${{ directory }}',
-          { directory: 'staging' },
-        ),
+        packageHandle.convertItem('<%= username %>/<%= directory %>', {
+          directory: 'staging',
+        }),
       ).toBe(null)
       expect(
+        packageHandle.convertItem('<%= username %>/<%= directory %>', {
+          directory: undefined,
+          username: undefined,
+        }),
+      ).toBe('/')
+    })
+
+    it('should treat null/undefined as an empty string', () => {
+      expect(
         packageHandle.convertItem(
-          // eslint-disable-next-line no-template-curly-in-string
-          '${{ username }}/${{ directory }}',
-          { directory: undefined, username: undefined },
+          'what-<%= username %>-do/make-<%= directory %>-update',
+          // @ts-expect-error
+          { directory: undefined, username: null },
         ),
-      ).toBe(null)
+      ).toBe('what--do/make--update')
     })
   })
 
   describe('convert', () => {
-    it('should use first successful replacement', () => {
+    it('should use contexted replacement', () => {
       expect(
-        // eslint-disable-next-line no-template-curly-in-string
-        packageHandle.convert(['${{ username }}/${{ directory }}', 'abc/def'], {
-          username: 'fiskus',
-        }),
+        packageHandle.convert(
+          { files: '<%= username %>/<%= directory %>', packages: 'abc/def' },
+          'packages',
+        ),
       ).toBe('abc/def')
       expect(
         packageHandle.convert(
-          // eslint-disable-next-line no-template-curly-in-string
-          ['${{ a }}/${{ b }}', '${{ username }}/${{ directory }}', 'abc/def'],
+          { files: '<%= username %>/<%= directory %>', packages: '<%= a %>/<%= b %>' },
+          'files',
           {
             username: 'fiskus',
             directory: 'staging',
           },
         ),
       ).toBe('fiskus/staging')
+    })
+
+    it('should return empty string if not enough values', () => {
       expect(
-        packageHandle.convert(
-          // eslint-disable-next-line no-template-curly-in-string
-          ['${{ username }}/${{ directory }}', '${{ username }}/', 'abc/def'],
-          {
-            username: 'fiskus',
-          },
-        ),
-      ).toBe('fiskus/')
+        packageHandle.convert({ files: '<%= username %>/<%= directory %>' }, 'files', {
+          username: 'fiskus',
+        }),
+      ).toBe('')
     })
   })
 })
--- catalog/app/utils/packageHandle.ts
+++ catalog/app/utils/packageHandle.ts
@@ -1,42 +1,26 @@
-interface Options {
+import lodashTemplate from 'lodash/template'
+
+type Context = 'files' | 'packages'
+
+type HandlesDict = Partial<Record<Context, string>>
+
+type Options = {
   username?: string
   directory?: string
 }
 
-const VALUE_TEMPLATE = /\$\{\{([^}]*)\}\}/
-
-export function getValueName(valueTemplate: string): string | null {
-  const found = valueTemplate.match(VALUE_TEMPLATE)
-  return found && found.length ? found[1].trim() : null
-}
-
-export function convertRegexItem(template: string, options?: Options): string | null {
-  if (!options) return template
-  const regexp = new RegExp(VALUE_TEMPLATE, 'g')
+export function convertItem(template: string, options?: Options): string | null {
   try {
-    return template.replace(regexp, (match) => {
-      const valueName = getValueName(match) as keyof Options
-      if (valueName && options[valueName] !== null && options[valueName] !== undefined)
-        return options[valueName]!
-      throw new Error('No match')
-    })
+    return lodashTemplate(template)(options)
   } catch (error) {
     return null
   }
 }
 
-export function convertItem(template: string, options?: Options): string | null {
-  if (template.includes('${{')) return convertRegexItem(template, options)
-  return template
-}
-
-export function convert(templates: string[], options?: Options): string {
-  let handle = ''
-  templates.some((template) => {
-    const converted = convertItem(template, options)
-    if (converted === null) return false
-    handle = converted
-    return true
-  })
-  return handle
+export function convert(
+  handlesDict: HandlesDict,
+  context: Context,
+  options?: Options,
+): string {
+  return convertItem(handlesDict[context] || '', options) || ''
 }
